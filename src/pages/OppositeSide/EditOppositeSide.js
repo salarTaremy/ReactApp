5a06549d7b2ff85b751c5379be550d3ddd1a6ba2
salar_taremy <salar_taremy@yahoo.com>
2022-05-24 16:19:41 +0430

    ComboBox

--- src/pages/OppositeSide/EditOppositeSide.js
+++ src/pages/OppositeSide/EditOppositeSide.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react"
-import { useForm } from "react-hook-form";
+import { useForm, Controller } from "react-hook-form";
+import ReactSelect from "react-select";
 import * as url from '../../helpers/url_helper'
 import { post, del, get, put } from "../../helpers/api_helper"
 import { useParams } from 'react-router-dom';
@@ -34,20 +35,23 @@ import { set } from "react-hook-form";
 const EditOppositeSide = () => {
   const [Th, SetTh] = useState(null);
   const [Cities, SetCities] = useState(null);
-  const { register, handleSubmit } = useForm({
+  const { register, handleSubmit, control } = useForm({
     defaultValues: { name: "Davood" }
   });
   const onSubmit = data => console.log(data);
 
   const { id } = useParams();
 
-
+  const getReactSelectDefault = async () => {
+    // Works (when not `async`):
+    return { value: "1", label: "User ID#1" };
+  };
 
 
   const UserForm = ({ _th }) => {
     console.log(_th);
 
-    const { register, handleSubmit } = useForm({
+    const { register, handleSubmit, methods } = useForm({
       defaultValues: _th
     });
 
@@ -55,6 +59,22 @@ const EditOppositeSide = () => {
       alert(JSON.stringify(data));
     };
 
+  
+
+    const Select = React.forwardRef(({ onChange, onBlur, name, label }, ref) => (
+      <>
+        <label>{label}</label>
+        <select name={name} ref={ref} onChange={onChange} onBlur={onBlur}>
+          <option value="1">ghabl</option>
+          <option value="1268">in 1268</option>
+          <option value="1026">in 1026</option>
+
+        </select>
+      </>
+    ));
+
+
+    const default_value = 1; // you can replace with your default value
     return (
       <form
         onSubmit={handleSubmit(onSubmit)}>
@@ -63,8 +83,12 @@ const EditOppositeSide = () => {
         <input {...register('sh_Sh')} className="form-control" />
         <input {...register('namePedar')} className="form-control" />
         <input {...register('code_melli')} className="form-control" />
+        <input {...register('iD_MahalTavalod')} className="form-control" />
+
+
+        <Select label="iD_MahalTavalod" {...register("iD_MahalTavalod")} />
+
         <input type="submit" className="btn btn-primary" />
-        
       </form>
 
     );
@@ -74,7 +98,6 @@ const EditOppositeSide = () => {
 
   // ////////////////////////////////////////////////////
   useEffect(() => {
-
     get(`${url.FETCH_Tarafhesab_Detail}/${id}`)
       .then((response) => {
         console.log(response.value)
@@ -82,24 +105,23 @@ const EditOppositeSide = () => {
       }, (error) => {
         console.error(error);
       });
-
-      get(url.GET_CITY)
+  }, []);
+  useEffect(() => {
+    get(url.GET_CITY)
       .then((response) => {
         console.log(response.value)
         SetCities(response.value)
       }, (error) => {
         console.error(error);
       });
-
   }, []);
-
 
 
   return (
     <React.Fragment>
       <div className="page-content">
         <Breadcrumbs title="عنوان" breadcrumbItem="ویرایش طرف حساب ها" />
-        {Th && Cities?
+        {Th && Cities ?
           <>
             <Card className="p-3">
               <p>{Th.name}</p>
