6f6d4f479f150fbae9d0e8a54bf0f7281f605c9e
salar_taremy <salar_taremy@yahoo.com>
2022-05-30 21:17:17 +0430

    ClasName text

--- src/pages/OppositeSide/EditOppositeSideForm.js
+++ src/pages/OppositeSide/EditOppositeSideForm.js
@@ -6,60 +6,67 @@ import { Card, } from "reactstrap"
 import NormalDropDown from "../../components/Common/NormalDropDown";
 
 const ThForm = (props) => {
-    const { register, handleSubmit, methods, control,watch } = useForm({
-      defaultValues: props.Th
-    });
-    const onSubmit = (data) => {
-      //typeof() result => array,object,null,number,string
-      if(typeof(data.iD_MahalTavalod) == 'object'){
-        data.iD_MahalTavalod = data.iD_MahalTavalod.id;
-      }
-      console.log(data)
-    };
+  //const textColor = "text-white"
+  const textColor = "text-secondary"
+  const { register, handleSubmit, methods, control, watch } = useForm({
+    defaultValues: props.Th
+  });
+  const onSubmit = (data) => {
+    //typeof() result => array,object,null,number,string
+    if (typeof (data.iD_MahalTavalod) == 'object') {
+      data.iD_MahalTavalod = data.iD_MahalTavalod.id;
+    }
+    console.log(data)
+  };
 
-    return (
-      <form
-        onSubmit={handleSubmit(onSubmit)}>
-        <input {...register('name')} className="form-control" />
-        <input {...register('famil')} className="form-control" />
-        <input {...register('sh_Sh')} className="form-control" />
-        <input {...register('namePedar')} className="form-control" />
-        <input {...register('code_melli')} className="form-control" />
+  return (
+    <form
+      onSubmit={handleSubmit(onSubmit)}>
+      <input {...register('name')} className="form-control" />
+      <input {...register('famil')} className="form-control" />
+      <input {...register('sh_Sh')} className="form-control" />
+      <input {...register('namePedar')} className="form-control" />
+      <input {...register('code_melli')} className="form-control" />
 
-        <Controller
-          control={control}
-          {...register('birthDay')}
-          render={({ field: { value, onChange } }) => (
-            <DatePicker className="form-control" onChange={onChange} value={value}></DatePicker>
-          )}
-        />
+      <Controller
+        control={control}
+        {...register('birthDay')}
+        render={({ field: { value, onChange } }) => (
+          <DatePicker
+            className="form-control"
+            onChange={onChange}
+            value={value}
+          />
+        )}
+      />
+      <Controller
+        control={control}
+        className="form-control text-danger"
+        {...register('iD_MahalTavalod')}
+        render={({
+          field: { onChange, onBlur, value, name, ref },
+          fieldState: { invalid, isTouched, isDirty, error },
+          formState,
+        }) => (
+          <Select
+            classNamePrefix={`${textColor} select2-selection`}
+            onBlur={onBlur} // notify when input is touched
+            onChange={onChange} // send value to hook form
+            inputRef={ref}
+            name={name}
+            options={props.Cities}
+            defaultValue={props.Cities.filter((item) => item.id === props.Th.iD_MahalTavalod)[0]}
+          />
+        )}
+      />
+      <NormalDropDown
+        className="form-control" {...register("iD_MahalSodoor")}
+        dataSource={props.Cities}
+      />
+      <input type="submit" className="btn btn-primary" />
+    </form>
+  );
+}
 
 
-
-        <Controller
-          control={control}
-          {...register('iD_MahalTavalod')}
-          render={({
-            field: { onChange , onBlur, value, name, ref },
-            fieldState: { invalid, isTouched, isDirty, error },
-            formState,
-          }) => (
-            <Select
-              onBlur={onBlur} // notify when input is touched
-              onChange={onChange } // send value to hook form
-              inputRef={ref}
-              name={name}
-              options={props.Cities}
-              defaultValue = {props.Cities.filter((item) => item.id === props.Th.iD_MahalTavalod)[0]}
-            />
-          )}
-        />
-
-        <NormalDropDown className="form-control" {...register("iD_MahalSodoor")} dataSource={props.Cities} />
-        <input type="submit" className="btn btn-primary" />
-      </form>
-    );
-  }
-
-
-  export default ThForm;
+export default ThForm;
