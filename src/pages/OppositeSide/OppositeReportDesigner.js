194efd39aefeb77ec1104b98d021c9447aac5c80
salar_taremy <salar_taremy@yahoo.com>
2022-08-24 17:54:54 +0430

    Ok Rep

--- src/pages/OppositeSide/OppositeReportDesigner.js
+++ src/pages/OppositeSide/OppositeReportDesigner.js
@@ -39,20 +39,43 @@ const Blank = () => {
     const dsDataSource = new Stimulsoft.System.Data.DataSet();
     var options = new Stimulsoft.Designer.StiDesignerOptions();
     //options.appearance.fullScreenMode = true;
+
+  
+    // options.viewerOptions.appearance.reportDisplayMode = Stimulsoft.Report.Export.StiHtmlExportMode.Auto;
+    // options.toolbar.showFileMenuExit = false;
+    // options.toolbar.showFileMenuOptions = false;
+    // options.bands.showChildBand = false;
+    // options.components.showPanel = false;
+    // options.appearance.showReportTree = false;
+    // options.appearance.showTooltips = false;
+    
+
+
      var designer = new Stimulsoft.Designer.StiDesigner(options, 'StiDesigner', false);
      var report = new Stimulsoft.Report.StiReport();
      //report.loadFile("/reports/crossTabTest2.mrt");
 
 
      const Ds = {
-      "value": [{"id": 2 , "name":"salar"}]
+      "value": [{"id": 1 , "name":"tar"},{"id": 2 , "name":"salar"}]
+      ,"val2": [{"id": 4 , "name":"aaaa"},{"id": 5 , "name":"bbbb"}]
+      ,"val3": "dasdas"
+      ,"val4": {"ffff" :11111111111}
   };
+
     dsDataSource.readJson(Ds);
     report.regData("DataSource", null, dsDataSource);
 
 
     designer.report = report;  
     designer.renderHtml("content");
+
+
+
+
+
+
+
 
 
 
