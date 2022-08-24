03f0ac10e5d7dd8065f4119a215d3bb45f5c16b4
salar_taremy <salar_taremy@yahoo.com>
2022-08-24 14:34:01 +0430

    ok

--- src/pages/OppositeSide/OppositeReport.js
+++ src/pages/OppositeSide/OppositeReport.js
@@ -1,20 +1,20 @@
 import React, { useState, useEffect } from "react"
 import {
-  Col,
-  Row,
-  Card,
-  CardBody,
-  CardTitle,
-  CardSubtitle,
-  CardImg,
-  CardText,
-  CardHeader,
-  CardImgOverlay,
-  CardFooter,
-  CardDeck,
-  CardColumns,
-  CardGroup,
-  Spinner,
+    Col,
+    Row,
+    Card,
+    CardBody,
+    CardTitle,
+    CardSubtitle,
+    CardImg,
+    CardText,
+    CardHeader,
+    CardImgOverlay,
+    CardFooter,
+    CardDeck,
+    CardColumns,
+    CardGroup,
+    Spinner,
 } from "reactstrap"
 
 
@@ -31,73 +31,56 @@ import { post, del, get, put } from "helpers/api_helper"
 //Url For Call Api
 import * as url from 'helpers/url_helper'
 
-const Page = ({cities}) => {
+const Page = ({ cities }) => {
 
     const Stimulsoft = window.Stimulsoft || {};
 
     // ////////////////////////////////////////////////////
-        useEffect(() => {
+    useEffect(() => {
+        const stiOptions = new Stimulsoft.Viewer.StiViewerOptions();
+        const viewer = new Stimulsoft.Viewer.StiViewer(stiOptions, "content_viewer", false);
+        viewer.renderHtml("content");
+        const report = new Stimulsoft.Report.StiReport();
+        report.loadFile("/reports/crossTabTest2.mrt");
+        const dsDataSource = new Stimulsoft.System.Data.DataSet();
+        const Ds = {
+            "value": cities.value
+        };
+        dsDataSource.readJson(Ds);
+        report.regData("DataSource", null, dsDataSource);
 
-
-            let stiOptions = new Stimulsoft.Viewer.StiViewerOptions();
-            const viewer = new Stimulsoft.Viewer.StiViewer(stiOptions, "content_viewer", false);
-            viewer.renderHtml("content");
-                let report = new Stimulsoft.Report.StiReport();
-                report.loadFile("/reports/crossTabTest2.mrt");
-                  var dsDataSource = new Stimulsoft.System.Data.DataSet();
-                  var dsDataSource2 = new Stimulsoft.System.Data.DataSet();
-    
-                  const cc = {
-                    "value": cities.value
-                };
-
-                    console.log('cc')
-                    console.log(cc)
-
-                dsDataSource.readJson(cc);
-                report.regData("DataSource",null,dsDataSource);
-
-                viewer.report = report;
+        viewer.report = report;
 
     }, []);
-        return (
-            <div id="content">
-            </div>
-        )
-    }
+    return (
+        <div id="content">
+        </div>
+    )
+}
 
 
-    const Main = () => {
-
-        // const data = {
-        //     "channels":[{"rt":"AAA","rc":83,"dc":"HC","dn":"HC","ic":"POINT","im":"POINT","v":"7.00"},
-        //     {"rt":"BBB","rc":83,"dc":"HC","dn":"HC","ic":"POINTPOINT","im":"POINT","v":"7.00"},
-        //     {"rt":"HHH","rc":83,"dc":"HC","dn":"HC","ic":"POINT","im":"POINT","v":"7.00"}]};
-
-
-        const [Cities, SetCities] = useState(null);
-        useEffect(() => {
-            get(url.GET_CITY)
-                .then((response) => {
-                    console.clear()
-                    console.log('response')
-                    //SetCities(response.value)                 
-                    SetCities(response) 
-                }, (error) => {
-                    console.error(error);
-                });
-        }, []);
-        return(
-            <React.Fragment>
+const Main = () => {
+    const [Cities, SetCities] = useState(null);
+    useEffect(() => {
+        get(url.GET_CITY)
+            .then((response) => {
+                console.clear()
+                console.log('response')
+                //SetCities(response.value)                 
+                SetCities(response)
+            }, (error) => {
+                console.error(error);
+            });
+    }, []);
+    return (
+        <React.Fragment>
             <div className="page-content">
                 <Breadcrumbs title="گزارش طرف حساب" breadcrumbItem="طرف حساب ها" />
-                                {Cities ?
-                    <Page  cities= {Cities}   ></Page>
+                {Cities ?
+                    <Page cities={Cities}   ></Page>
                     : <Loading />}
-
             </div>
         </React.Fragment>
-        )
-    }
-   
-    export default Main
+    )
+}
+export default Main
