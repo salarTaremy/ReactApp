8c8b29c3ec99e3daccf110986451ded855b99be9
salar_taremy <salar_taremy@yahoo.com>
2022-08-25 09:13:17 +0430

    Font Ok

--- src/pages/OppositeSide/OppositeReportDesigner.js
+++ src/pages/OppositeSide/OppositeReportDesigner.js
@@ -32,15 +32,15 @@ import { post, del, get, put } from "helpers/api_helper"
 import * as url from 'helpers/url_helper'
 
 
-const Blank = ({cities}) => {
-    const Stimulsoft = window.Stimulsoft || {};
+const Blank = ({ cities }) => {
+  const Stimulsoft = window.Stimulsoft || {};
   useEffect(() => {
-   
+
     const dsDataSource = new Stimulsoft.System.Data.DataSet();
     var options = new Stimulsoft.Designer.StiDesignerOptions();
     //options.appearance.fullScreenMode = true;
 
-  
+
     // options.viewerOptions.appearance.reportDisplayMode = Stimulsoft.Report.Export.StiHtmlExportMode.Auto;
     // options.toolbar.showFileMenuExit = false;
     // options.toolbar.showFileMenuOptions = false;
@@ -48,45 +48,34 @@ const Blank = ({cities}) => {
     // options.components.showPanel = false;
     // options.appearance.showReportTree = false;
     // options.appearance.showTooltips = false;
-    
 
 
-     var designer = new Stimulsoft.Designer.StiDesigner(options, 'StiDesigner', false);
-     var report = new Stimulsoft.Report.StiReport();
-     //report.loadFile("/reports/crossTabTest2.mrt");
 
-
+    var designer = new Stimulsoft.Designer.StiDesigner(options, 'StiDesigner', false);
+    var report = new Stimulsoft.Report.StiReport();
+    //report.loadFile("/reports/crossTabTest2.mrt");
 
     dsDataSource.readJson(cities);
     report.regData("DataSource", null, dsDataSource);
 
 
-    designer.report = report;  
+    designer.report = report;
     designer.renderHtml("content");
 
 
 
-
-
-
-
-
-
-
-
-    
   }, []);
 
 
   return (
-  <>
-  
-  <style>{".ltr{direction: ltr;}"}</style>
-   
-   <div   className="ltr"  
-     id="content"></div>
-  
-  </>
+    <>
+
+      <style>{".ltr{direction: ltr;}"}</style>
+
+      <div className="ltr"
+        id="content"></div>
+
+    </>
   )
 }
 
@@ -95,25 +84,25 @@ const Blank = ({cities}) => {
 const Main = () => {
   const [Cities, SetCities] = useState(null);
   useEffect(() => {
-      get(url.GET_CITY)
-          .then((response) => {
-              console.clear()
-              console.log('response')
-              //SetCities(response.value)                 
-              SetCities(response)
-          }, (error) => {
-              console.error(error);
-          });
+    get(url.GET_CITY)
+      .then((response) => {
+        console.clear()
+        console.log('response')
+        //SetCities(response.value)                 
+        SetCities(response)
+      }, (error) => {
+        console.error(error);
+      });
   }, []);
   return (
-      <React.Fragment>
-          <div className="page-content">
-              <Breadcrumbs title="گزارش طرف حساب" breadcrumbItem="طرف حساب ها" />
-              {Cities ?
-                  <Blank cities={Cities}   ></Blank>
-                  : <Loading />}
-          </div>
-      </React.Fragment>
+    <React.Fragment>
+      <div className="page-content">
+        <Breadcrumbs title="گزارش طرف حساب" breadcrumbItem="طرف حساب ها" />
+        {Cities ?
+          <Blank cities={Cities}   ></Blank>
+          : <Loading />}
+      </div>
+    </React.Fragment>
   )
 }
 export default Main
