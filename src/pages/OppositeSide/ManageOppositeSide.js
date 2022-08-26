4ac753babd1b12da50d0534caa3ef982fa1e31bc
salar_taremy <salar_taremy@yahoo.com>
2022-08-26 16:33:27 +0430

    Report Btn

--- src/pages/OppositeSide/ManageOppositeSide.js
+++ src/pages/OppositeSide/ManageOppositeSide.js
@@ -4,11 +4,11 @@ import { Link, Route, useHistory } from "react-router-dom"
 import Loading from "components/Common/Loading"
 import DatePicker from "components/Common/DatePicker"
 //import { useForm } from "react-hook-form";
-import { Card } from "reactstrap"
+import { Card, Modal } from "reactstrap"
 import Breadcrumbs from "components/Common/Breadcrumb"
 import GridView from "components/Grid/GridView"
 import * as url from 'helpers/url_helper'
-import { post,del, get, put } from "helpers/api_helper"
+import { post, del, get, put } from "helpers/api_helper"
 
 let CityList = [];
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -25,7 +25,6 @@ const ManageOppositeSide = () => {
         console.error(error);
       });
   }, []);
-
   return (
     <React.Fragment>
       <div className="page-content">
@@ -39,9 +38,80 @@ const ManageOppositeSide = () => {
             <Card className="p-1">
               <GridView Url={url.FETCH_OPPOSITE_SIDE} Columns={cols.OPPOSITE_SIDE_COLUMNS} />
             </Card>
+            <Card >
+              <Report></Report>
+            </Card>
+            <Card >
+              <Report></Report>
+            </Card>
+
           </>}
       </div>
     </React.Fragment>
   )
 }
 export default ManageOppositeSide
+
+
+
+
+
+
+const Report = () => {
+  const [modal_center, setmodal_center] = useState(false)
+  const tog_center = () => {
+    setmodal_center(!modal_center)
+    removeBodyCss()
+  }
+  const removeBodyCss = () => {
+    document.body.classList.add("no_padding")
+  }
+  const BtnClose = () => {
+    return (
+      <button
+        type="button"
+        onClick={() => {
+          setmodal_center(false)
+        }}
+        className="close"
+        data-dismiss="modal"
+        aria-label="Close"
+      >
+        <span aria-hidden="true">&times;</span>
+      </button>
+    )
+  }
+  return (
+
+    <>
+      <Modal
+        isOpen={modal_center}
+        toggle={tog_center}
+        centered={true}
+      >
+        <div className="modal-header">
+          <h5 className="modal-title mt-0">گزارش</h5>
+          <BtnClose></BtnClose>
+        </div>
+        <div className="modal-body">
+          <p>OK</p>
+        </div>
+      </Modal>
+
+      <div>
+        <button
+          type="button"
+          className="btn btn-primary waves-effect waves-light"
+          onClick={() => {
+            tog_center()
+          }}
+          data-toggle="modal"
+          data-target=".bs-example-modal-center"
+        >
+          چاپ گزارشات
+        </button>
+      </div>
+    </>
+
+  )
+}
